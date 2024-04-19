df06cba54443a618fb45b5be21d4976a11f6cb63
Kriti <kumarikriti760@gmail.com>
2024-04-19 11:23:28 +0530

    first commit

---
+++ Rideit/src/test/java/com/example/Rideit/RideitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rideit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
