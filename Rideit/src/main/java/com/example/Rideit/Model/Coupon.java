6dc4d2b226f73fe51208fc675807b406c7c64564
Kriti Kumari <157258583+kriti8kum@users.noreply.github.com>
2024-04-21 21:54:00 +0530

    Coupon.java

--- Rideit/src/main/java/com/example/Rideit/Model/Coupon.java
+++ Rideit/src/main/java/com/example/Rideit/Model/Coupon.java
@@ -1,21 +1,32 @@
 package com.example.Rideit.Model;
+import com.example.Rideit.Enum.Gender;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Coupon {
+@Table(name = "customer")
+public class Customer {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
-    String couponCode;
-    int percantageDiscount;
+    String name;
+    int age;
+    @Column(unique = true, nullable = false)
+    String emailId;
+    String address;
+    @Enumerated(EnumType.STRING)
+    Gender gender;
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    List<TripBooking> bookings = new ArrayList<>();
 
 
 }
-
