29d48ba88ebce690b65297e1a991a5ccede96849
Kriti Kumari <157258583+kriti8kum@users.noreply.github.com>
2024-04-21 21:52:55 +0530

    Coupon.java

--- Rideit/src/main/java/com/example/Rideit/Model/Coupon.java
+++ Rideit/src/main/java/com/example/Rideit/Model/Coupon.java
@@ -1,2 +1,21 @@
-package com.example.Rideit.Model;public class Coupon {
+package com.example.Rideit.Model;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Coupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String couponCode;
+    int percantageDiscount;
+
+
 }
+
