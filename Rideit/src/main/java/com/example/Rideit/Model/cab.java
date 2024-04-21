c3e6912bfd41602e67df061c213deb81df512cfd
Kriti Kumari <157258583+kriti8kum@users.noreply.github.com>
2024-04-21 21:51:50 +0530

    cab.java

--- Rideit/src/main/java/com/example/Rideit/Model/cab.java
+++ Rideit/src/main/java/com/example/Rideit/Model/cab.java
@@ -1,2 +1,31 @@
-package com.example.Rideit.Model;public class cab {
+package com.example.Rideit.Model;
+import com.example.Rideit.Enum.CarType;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public class cab {
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String company;
+
+    String carModel;
+    @Enumerated(EnumType.STRING)
+    CarType carType;
+
+    int numberOfSeats;
+
+    double farePerKm;
+    @OneToOne
+    @JoinColumn
+    Driver driver;
+
 }
