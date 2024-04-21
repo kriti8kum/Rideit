2a496b4a4599fa4d50221d4dd7fb24d7a399c131
Kriti Kumari <157258583+kriti8kum@users.noreply.github.com>
2024-04-21 21:56:13 +0530

    TripBooking.java

--- Rideit/src/main/java/com/example/Rideit/Model/TripBooking.java
+++ Rideit/src/main/java/com/example/Rideit/Model/TripBooking.java
@@ -1,2 +1,35 @@
-package com.example.Rideit.Model;public class TripBooking {
+package com.example.Rideit.Model;
+import com.example.Rideit.Enum.TripStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TripBooking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    String source;
+    String destination;
+    double tripDistanceInKm;
+    double totalFare;
+    TripStatus tripStatus;
+    @CreationTimestamp
+    Date bookedAt;
+
+    @ManyToOne
+    @JoinColumn
+    Customer customer;
+
+    @ManyToOne
+    @JoinColumn
+    Driver driver;
+
 }
