892b21ee2a05aecbf0c64b339d3063c4fef3dbef
Kriti Kumari <157258583+kriti8kum@users.noreply.github.com>
2024-04-21 21:55:21 +0530

    Driver.java

--- Rideit/src/main/java/com/example/Rideit/Model/Driver.java
+++ Rideit/src/main/java/com/example/Rideit/Model/Driver.java
@@ -1,2 +1,30 @@
-package com.example.Rideit.Model;public class Driver {
+package com.example.Rideit.Model;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+    String panNumber;
+    int age;
+    @Column(unique = true,nullable =false)
+    String mobNo;
+    double rating;
+    @OneToOne(mappedBy = "driver", cascade = CascadeType.ALL)
+    cab cab;
+
+    @OneToMany(mappedBy = "driver",cascade = CascadeType.ALL)
+    List<TripBooking> bookings = new ArrayList<>();
+
 }
